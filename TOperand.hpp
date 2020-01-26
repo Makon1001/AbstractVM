43e5e580f155adbc395b0cca976cde8928b1c86f
Manuel Konrath <manuel.konrath@gmail.com>
2020-01-26 16:21:37 +0100

    add TOperand

---
+++ TOperand.hpp
@@ -0,0 +1,13 @@
+//
+//  TOperand.hpp
+//  AbstractVM
+//
+//  Created by Manuel Konrath on 25/01/2020.
+//  Copyright © 2020 Manuel Konrath. All rights reserved.
+//
+
+#ifndef TOperand_h
+#define TOperand_h
+
+
+#endif /* TOperand_h */
